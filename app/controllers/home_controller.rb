e31b9eb7868be1ea99eac664147e1ef1bcc73281
David Pick <pickdavid@gmail.com>
2012-12-30 14:37:16 -0600

    clean up the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     if current_user
-      @recipes = Recipe.where(:user_id => current_user.id).last(10)
+      redirect_to recipes_path
     else
       @recipes = Recipe.last(10)
     end
