bcc5c0a576cf9298d1216e9cd8a4a40a14b26de1
David Pick <pickdavid@gmail.com>
2013-01-01 20:54:53 -0600

    make sure usernames are unique and only alphanumeric

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
 
   friendly_id :username, :use => :slugged
 
+  validates :username, :uniqueness => true
+  validates_format_of :username, :with => /^[a-zA-Z0-9_]*$/, :on => :create
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
