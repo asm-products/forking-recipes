ca2bb804fad440422b12ca07d57581a2499c4aa5
David Pick <pickdavid@gmail.com>
2013-01-13 20:22:47 -0600

    fixing the users query for the browse page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,10 +16,10 @@ class HomeController < ApplicationController
 
   def browse
     @recipes = Recipe.where(:forked_from_recipe_id => nil).last(10)
-    @users   = User.all(:select => "users.*, COUNT(recipes.user_id) as purchased_sum",
+    @users   = User.all(:select => "users.*, COUNT(recipes.user_id) as recipe_count",
                         :joins  => "LEFT JOIN recipes ON recipes.user_id = users.id",
                         :group  => "users.id",
-                        :order  => "users.created_at",
+                        :order  => "recipe_count DESC",
                         :limit  => 10)
   end
 end
