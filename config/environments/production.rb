0bcf6819af61475defa6db3f224c4831d5c96629
David Pick <pickdavid@gmail.com>
2013-12-02 09:19:30 +0000

    let rails serve assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ RecipeHub::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "d32jhbiktiamm7.cloudfront.net"
+  # config.action_controller.asset_host = "d32jhbiktiamm7.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
