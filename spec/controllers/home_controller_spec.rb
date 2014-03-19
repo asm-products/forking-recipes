5a9254e64951e54d081d017c2d41126a54a17106
David Pick <pickdavid@gmail.com>
2014-03-19 18:30:09 -0500

    removing all the autogenerated specs

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,12 +1,4 @@
 require 'spec_helper'
 
 describe HomeController do
-
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.code.should == '302'
-    end
-  end
-
 end
