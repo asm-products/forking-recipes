2cecfdf82e3e835303be13de456307ad44bd7a55
David Pick <pickdavid@gmail.com>
2014-03-23 20:19:22 -0500

    fixing the sidebar list of recipes when logged in and viewing another users recipes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,13 @@ class ApplicationController < ActionController::Base
 
   def set_up_side_bar_variables
     if current_user
-      @recipes = Recipe.where(:user_id => current_user.id)
+      @layout_recipes = Recipe.where(:user_id => current_user.id)
     else
-      @users = User.all(:select => "users.*, COUNT(recipes.user_id) as recipe_count",
-                 :joins  => "LEFT JOIN recipes ON recipes.user_id = users.id",
-                 :group  => "users.id",
-                 :order  => "recipe_count DESC",
-                 :limit  => 10)
+      @layout_users = User.all(:select => "users.*, COUNT(recipes.user_id) as recipe_count",
+                               :joins  => "LEFT JOIN recipes ON recipes.user_id = users.id",
+                               :group  => "users.id",
+                               :order  => "recipe_count DESC",
+                               :limit  => 10)
     end
   end
 end
