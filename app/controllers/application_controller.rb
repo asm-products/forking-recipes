09633e014be914745ce625e79d34f004bf4f5834
David Pick <pickdavid@gmail.com>
2014-04-08 20:31:54 -0500

    adding a see more recipes button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_filter :set_up_side_bar_variables
   protect_from_forgery
-
-  def set_up_side_bar_variables
-    if current_user
-      @layout_recipes = Recipe.where(:user_id => current_user.id)
-    else
-      @layout_users = User.all(:select => "users.*, COUNT(recipes.user_id) as recipe_count",
-                               :joins  => "LEFT JOIN recipes ON recipes.user_id = users.id",
-                               :group  => "users.id",
-                               :order  => "recipe_count DESC",
-                               :limit  => 10)
-    end
-  end
 end
