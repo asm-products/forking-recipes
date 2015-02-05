267b56c98cbbca5992ef05998a10e3b9276d4b7a
Philipp Wesner <pwesner@innomind.info>
2015-02-05 19:06:48 +0100

    fixed broken specs; added recipe partial and used it in home, browse, landing and my recipes view

--- spec/controllers/recipe_revisions_controller_spec.rb
+++ spec/controllers/recipe_revisions_controller_spec.rb
@@ -11,7 +11,7 @@ describe RecipeRevisionsController do
 
       get :show, {recipe_id: recipe.to_param, user_id: user.to_param, id: recipe_revision.to_param}
       expect(assigns(:revision)).to eq(recipe_revision)
-      expect(assigns(:target_revision)).to eq(previous_revision)
+      expect(assigns(:target_revision)).to eq(recipe_revision2)
       expect(assigns(:diff)).to eq(Differ.diff_by_line(recipe_revision.body, recipe_revision2.body).format_as(:html).html_safe)
     end
 
@@ -19,8 +19,8 @@ describe RecipeRevisionsController do
       recipe = FactoryGirl.create(:recipe)
       user = recipe.user
       recipe_revision = FactoryGirl.create(:recipe_revision, revision: 2, user_id: user.id, recipe_id: recipe.id)
-      user2 = recipe2.user
       recipe2 = FactoryGirl.create(:recipe)
+      user2 = recipe2.user
       recipe_revision2 = FactoryGirl.create(:recipe_revision, user_id: user2.id, recipe_id: recipe2.id)
 
       get :show, {recipe_id: recipe.to_param, user_id: user.to_param, id: recipe_revision.to_param, target_id: recipe_revision2.to_param}
@@ -30,3 +30,4 @@ describe RecipeRevisionsController do
     end
   end
 end
+
